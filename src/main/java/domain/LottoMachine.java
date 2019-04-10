7a0b0e2c9d2a2e30f472a43f6eeb1e2ab30f6b68
os94 <ss1511@naver.com>
2019-04-10 16:24:16 +0900

    Add User class
    - Input winningLotto and bonusNo

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -3,12 +3,19 @@ package domain;
 public class LottoMachine {
     private User user = new User();
     private int inputMoney;
+    private WinningLotto winningLotto;
 
     public void run() {
         setInputMoney();
+
+        setWinningLotto();
     }
 
     private void setInputMoney() {
         this.inputMoney = user.inputMoney();
+    }
+
+    private void setWinningLotto() {
+        this.winningLotto = new WinningLotto(user.inputWinningLotto(), user.inputBonusNo());
     }
 }
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,9 +1,13 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class User {
     private final static String GUIDE_USER_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private final static String GUIDE_USER_INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
+    private final static String GUIDE_USER_INPUT_BONUS_NO = "보너스 볼을 입력해 주세요.";
     private Scanner sc = new Scanner(System.in);
 
     public int inputMoney() {
@@ -11,6 +15,39 @@ public class User {
 
         do {
             System.out.println(GUIDE_USER_INPUT_MONEY);
+            input = sc.nextLine();
+        } while (false);
+
+        return Integer.parseInt(input);
+    }
+
+    public Lotto inputWinningLotto() {
+        String input;
+
+        do {
+            System.out.println(GUIDE_USER_INPUT_WINNING_LOTTO);
+            input = sc.nextLine();
+        } while (false);
+
+        return parseLotto(input);
+    }
+
+    private Lotto parseLotto(String input) {
+        List<Integer> numberList = new ArrayList<>();
+
+        input = input.replace(" ", "");
+        for (String number : input.split(",")) {
+            numberList.add(Integer.parseInt(number));
+        }
+
+        return new Lotto(numberList);
+    }
+
+    public int inputBonusNo() {
+        String input;
+
+        do {
+            System.out.println(GUIDE_USER_INPUT_BONUS_NO);
             input = sc.nextLine();
         } while (false);
 
