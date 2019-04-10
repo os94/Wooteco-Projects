5f2d2723fecacf97e7cdc27d869eb2e3c04052e0
os94 <ss1511@naver.com>
2019-04-11 00:25:44 +0900

    Add printEachResult(), getNumberOfRank()

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -6,6 +6,8 @@ public class LottoMachine {
     private static final String EXIT_MESSAGE_LACK_OF_MONEY = "금액이 부족합니다. 게임을 종료합니다.";
     private static final String PRINT_MESSAGE_BUY_LOTTO = "개를 구매했습니다.";
     private static final String PRINT_MESSAGE_MATCH_RESULT = "\n당첨 통계\n----------";
+    private static final String PRINT_MESSAGE_EACH_RESULT = "%d개 일치 (%d원)- %d개\n";
+    private static final String PRINT_MESSAGE_EACH_RESULT_SECOND = "%d개 일치, 보너스볼 일치(%d원)- %d개\n";
     private static final String PRINT_MESSAGE_PROFIT = "총 수익률은 %.3f입니다.\n";
     private static final int PRICE_OF_LOTTO = 1000;
     private static final int STANDARD_NUMBER_OF_LOTTO = 6;
@@ -84,13 +86,27 @@ public class LottoMachine {
     }
 
     private void printResult() {
+        Rank[] rankValues = Rank.values();
+
         System.out.println(PRINT_MESSAGE_MATCH_RESULT);
-        printEachResult();
+        for (int i = rankValues.length - 2; i >= 0; i--) {
+            printEachResult(rankValues[i]);
+        }
         System.out.printf(PRINT_MESSAGE_PROFIT, getProfit());
     }
 
-    private void printEachResult() {
-        
+    private void printEachResult(Rank rank) {
+        if (rank == Rank.SECOND) {
+            System.out.printf(PRINT_MESSAGE_EACH_RESULT_SECOND
+                    , rank.getCountOfMatch(), rank.getWinningMoney(), getNumberOfRank(rank));
+            return;
+        }
+        System.out.printf(PRINT_MESSAGE_EACH_RESULT
+                , rank.getCountOfMatch(), rank.getWinningMoney(), getNumberOfRank(rank));
+    }
+
+    private int getNumberOfRank(Rank findingRank) {
+        return Collections.frequency(ranks, findingRank);
     }
 
     private double getProfit() {
