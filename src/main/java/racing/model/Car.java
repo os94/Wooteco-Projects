e1cc008efeb9230449355c6ca448ab632e637f4b
os94 <ss1511@naver.com>
2019-05-09 16:06:41 +0900

    Refact : Car constructor

--- src/main/java/racing/model/Car.java
+++ src/main/java/racing/model/Car.java
@@ -10,11 +10,6 @@ public class Car {
         this.name = name;
     }
 
-    public Car(String name, int position) {
-        this.name = name;
-        this.position = position;
-    }
-
     public String getName() {
         return name;
     }
--- src/test/java/racing/model/CarTest.java
+++ src/test/java/racing/model/CarTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
     @Test
     void 경계값에_따른_차량이동() {
-        Car car = new Car("pobi", 0);
+        Car car = new Car("pobi");
         car.move(3);
         assertThat(car.getPosition()).isEqualTo(0);
 
