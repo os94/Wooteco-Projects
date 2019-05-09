6f82cf03fd5178049493ef3420d0017b91be38f2
os94 <ss1511@naver.com>
2019-05-09 14:11:05 +0900

    Add Function : determine winner

--- src/main/java/racing/model/Winner.java
+++ src/main/java/racing/model/Winner.java
@@ -1,8 +1,34 @@
 package racing.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
+    private List<Car> cars;
+
     public Winner(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getWinners() {
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            determineWinner(winners, car);
+        }
+        return winners;
+    }
+
+    private int getMaxDistance() {
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(max, car.getPosition());
+        }
+        return max;
+    }
+
+    private void determineWinner(List<Car> winners, Car car) {
+        if (car.isMaxPosition(getMaxDistance())) {
+            winners.add(car);
+        }
     }
 }
