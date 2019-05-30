24b98a185a58c0177563e11b86ff355dede3bb8a
os94 <ss1511@naver.com>
2019-05-30 17:00:58 +0900

    [REF] InputView 중복제거

--- src/main/java/lotto/ConsoleLottoApplication.java
+++ src/main/java/lotto/ConsoleLottoApplication.java
@@ -7,9 +7,14 @@ import lotto.view.OutputView;
 import java.util.List;
 
 public class ConsoleLottoApplication {
+    private static final String MESSAGE_MONEY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_COUNT_OF_MANUAL_LOTTO = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MESSAGE_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_BONUS_NO = "보너스 볼을 입력해 주세요.";
+
     public static void main(String[] args) {
-        Money money = new Money(InputView.inputMoney());
-        Positive countOfManualLotto = new Positive(InputView.inputCountOfManualLotto());
+        Money money = new Money(InputView.inputPositiveNumber(MESSAGE_MONEY));
+        Positive countOfManualLotto = new Positive(InputView.inputPositiveNumber(MESSAGE_COUNT_OF_MANUAL_LOTTO));
         Positive countOfAutoLotto = money.getCountOfLotto().subtract(countOfManualLotto);
         List<String> lottos = inputManualLotto(countOfManualLotto);
 
@@ -22,8 +27,8 @@ public class ConsoleLottoApplication {
 
         try {
             WinningLotto winningLotto = new WinningLotto(
-                    LottoGenerator.generate(InputView.inputWinningLotto()),
-                    new LottoNumber(InputView.inputBonusNo()));
+                    LottoGenerator.generate(InputView.inputLotto(MESSAGE_WINNING_LOTTO)),
+                    new LottoNumber(InputView.inputPositiveNumber(MESSAGE_BONUS_NO)));
         } catch (Exception e) {
             System.err.println(e);
         }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,48 +5,24 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요.";
-    private static final String MESSAGE_INPUT_COUNT_OF_MANUAL_LOTTO = "수동으로 구매할 로또 수를 입력해 주세요.";
-    private static final String MESSAGE_INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
-    private static final String MESSAGE_INPUT_BONUS_NO = "보너스 볼을 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputMoney() {
-        System.out.println(MESSAGE_INPUT_MONEY);
+    public static int inputPositiveNumber(String message) {
+        System.out.println(message);
         try {
             return Integer.parseInt(scanner.nextLine());
         } catch (IllegalArgumentException e) {
             System.err.println(e);
-            return inputMoney();
+            return inputPositiveNumber(message);
         }
     }
 
-    public static int inputCountOfManualLotto() {
-        System.out.println(MESSAGE_INPUT_COUNT_OF_MANUAL_LOTTO);
-        try {
-            return Integer.parseInt(scanner.nextLine());
-        } catch (IllegalArgumentException e) {
-            System.err.println(e);
-            return inputCountOfManualLotto();
-        }
-    }
-
-    public static String inputWinningLotto() {
-        System.out.println(MESSAGE_INPUT_WINNING_LOTTO);
+    public static String inputLotto(String message) {
+        System.out.println(message);
         String input = scanner.nextLine();
         if (StringUtils.isBlank(input)) {
-            return inputWinningLotto();
+            return inputLotto(message);
         }
         return input;
-    }
-
-    public static int inputBonusNo() {
-        System.out.println(MESSAGE_INPUT_BONUS_NO);
-        try {
-            return Integer.parseInt(scanner.nextLine());
-        } catch (IllegalArgumentException e) {
-            System.err.println(e);
-            return inputBonusNo();
-        }
     }
 }
