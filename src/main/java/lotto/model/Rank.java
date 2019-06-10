aecb36c29e5ddc67bf22c522fb388b373a641ac0
os94 <ss1511@naver.com>
2019-06-10 21:07:21 +0900

    [MOD] Rank.valueOf 리팩토링

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,7 +1,6 @@
 package lotto.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
@@ -11,20 +10,8 @@ public enum Rank {
     FIFTH(3, 5_000),
     MISS(0, 0);
 
-    private static final String ERROR_INVALID_RANK = "는 유효하지 않은 값입니다.";
-    private static final Map<Integer, Rank> ranks = new HashMap<>();
-
     private int countOfMatch;
     private int prizeMoney;
-
-    static {
-        ranks.put(6, FIRST);
-        ranks.put(4, FOURTH);
-        ranks.put(3, FIFTH);
-        ranks.put(2, MISS);
-        ranks.put(1, MISS);
-        ranks.put(0, MISS);
-    }
 
     Rank(int countOfMatch, int prizeMoney) {
         this.countOfMatch = countOfMatch;
@@ -44,16 +31,14 @@ public enum Rank {
     }
 
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
-        if (ranks.containsKey(countOfMatch)) {
-            return ranks.get(countOfMatch);
-        }
-        if (SECOND.matchCount(countOfMatch) && matchBonus) {
-            return SECOND;
-        }
         if (THIRD.matchCount(countOfMatch) && !matchBonus) {
             return THIRD;
         }
-        throw new IllegalArgumentException(countOfMatch + ERROR_INVALID_RANK);
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount(countOfMatch))
+                .findFirst()
+                .orElse(MISS)
+                ;
     }
 
     private boolean matchCount(int countOfMatch) {
