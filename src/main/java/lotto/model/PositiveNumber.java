5345d94a76b0854cb3e04e35916f15cbaca6797d
os94 <ss1511@naver.com>
2019-06-11 20:42:59 +0900

    [ADD] 웹UI 기본적인 기능 구현

--- src/main/java/lotto/model/PositiveNumber.java
+++ src/main/java/lotto/model/PositiveNumber.java
@@ -44,4 +44,9 @@ public class PositiveNumber {
     public int hashCode() {
         return Objects.hash(number);
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
 }
---
+++ src/main/java/lotto/view/WebViewBuilder.java
@@ -0,0 +1,34 @@
+package lotto.view;
+
+import lotto.model.GameResult;
+import lotto.model.Lottos;
+import lotto.model.Rank;
+
+import java.util.stream.Collectors;
+
+public class WebViewBuilder {
+
+    public static String of(Lottos lottos) {
+        return lottos.getLottos().stream()
+                .map(lotto -> lotto + "<br>")
+                .collect(Collectors.joining());
+    }
+
+    public static String of(GameResult gameResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("당첨 통계").append("<br>");
+
+        Rank[] rankValues = Rank.values();
+        for (int i = rankValues.length - 2; i >= 0; i--) {
+            if (Rank.SECOND == rankValues[i]) {
+                stringBuilder.append(rankValues[i].getCountOfMatch()).append("개 일치, 보너스볼 일치, ")
+                        .append(rankValues[i].getPrizeMoney()).append("원) : ").append(gameResult.get(rankValues[i])).append("개 <br>");
+                continue;
+            }
+            stringBuilder.append(rankValues[i].getCountOfMatch()).append("개 일치, ")
+                    .append(rankValues[i].getPrizeMoney()).append("원) : ").append(gameResult.get(rankValues[i])).append("개 <br>");
+        }
+
+        return stringBuilder.toString();
+    }
+}
