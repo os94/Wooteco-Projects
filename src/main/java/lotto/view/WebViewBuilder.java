5da8f71351d2614bee12621894f325c2e3dfc55b
os94 <ss1511@naver.com>
2019-06-12 10:59:33 +0900

    [ADD] index/result.html CSS 추가

--- src/main/java/lotto/view/WebViewBuilder.java
+++ src/main/java/lotto/view/WebViewBuilder.java
@@ -16,17 +16,16 @@ public class WebViewBuilder {
 
     public static String of(GameResult gameResult) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("당첨 통계").append("<br>");
 
         Rank[] rankValues = Rank.values();
         for (int i = rankValues.length - 2; i >= 0; i--) {
             if (Rank.SECOND == rankValues[i]) {
                 stringBuilder.append(rankValues[i].getCountOfMatch()).append("개 일치, 보너스볼 일치, ")
-                        .append(rankValues[i].getPrizeMoney()).append("원) : ").append(gameResult.get(rankValues[i])).append("개 <br>");
+                        .append(rankValues[i].getPrizeMoney()).append("원 : ").append(gameResult.get(rankValues[i])).append("개 <br>");
                 continue;
             }
             stringBuilder.append(rankValues[i].getCountOfMatch()).append("개 일치, ")
-                    .append(rankValues[i].getPrizeMoney()).append("원) : ").append(gameResult.get(rankValues[i])).append("개 <br>");
+                    .append(rankValues[i].getPrizeMoney()).append("원 : ").append(gameResult.get(rankValues[i])).append("개 <br>");
         }
 
         return stringBuilder.toString();
