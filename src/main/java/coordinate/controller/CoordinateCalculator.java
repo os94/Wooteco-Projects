16320a3f1f3a7c82de5a45a19947e8a7deec8f1b
os94 <ss1511@naver.com>
2019-06-16 18:25:08 +0900

    [REF] Point 만드는부분을 컨트롤러로 옮겨 InputView 의존성 제거

--- src/main/java/coordinate/controller/CoordinateCalculator.java
+++ src/main/java/coordinate/controller/CoordinateCalculator.java
@@ -1,18 +1,49 @@
 package coordinate.controller;
 
+import coordinate.Message;
 import coordinate.model.Figure;
 import coordinate.model.FigureFactory;
+import coordinate.model.Point;
 import coordinate.view.InputView;
 import coordinate.view.OutputView;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class CoordinateCalculator {
+    private static final Pattern PATTERN_POINT = Pattern.compile("\\(([0-9]{1,2}),([0-9]{1,2})\\)");
+    private static final String POINT_DELIMITER = "-";
+
     public void run() {
         try {
-            Figure figure = FigureFactory.create(InputView.inputCoordinates());
+            String inputPoints = InputView.inputCoordinates();
+            Figure figure = FigureFactory.create(generatePoints(inputPoints));
             OutputView.showCoordinatePlane(figure.getXYCoordinates());
             OutputView.print(figure.getAreaInfo());
         } catch (Exception e) {
             System.err.println(e);
         }
+    }
+
+    private List<Point> generatePoints(String input) {
+        String[] inputPoints = input.split(POINT_DELIMITER);
+
+        return Arrays.stream(inputPoints)
+                .map(this::generatePoint)
+                .collect(Collectors.toList())
+                ;
+    }
+
+    private Point generatePoint(String inputPoint) {
+        Matcher matcher = PATTERN_POINT.matcher(inputPoint);
+        if (matcher.find()) {
+            int x = Integer.parseInt(matcher.group(1));
+            int y = Integer.parseInt(matcher.group(2));
+            return new Point(x, y);
+        }
+        throw new IllegalArgumentException(Message.ERROR_INVALID_COORDINATES);
     }
 }
--- src/main/java/coordinate/view/InputView.java
+++ src/main/java/coordinate/view/InputView.java
@@ -1,32 +1,22 @@
 package coordinate.view;
 
 import coordinate.Message;
-import coordinate.model.Point;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String POINT_DELIMITER = "-";
     private static Scanner scanner = new Scanner(System.in);
     private static final Pattern PATTERN_MULTIPLE_POINTS = Pattern.compile("(\\([0-9]{1,2},[0-9]{1,2}\\))(-(\\([0-9]{1,2},[0-9]{1,2}\\))){0,3}");
-    private static final Pattern PATTERN_POINT = Pattern.compile("\\(([0-9]{1,2}),([0-9]{1,2})\\)");
 
-    public static List<Point> inputCoordinates() {
+    public static String inputCoordinates() {
         System.out.println(Message.INPUT_COORDINATE);
-        return inputCoordinates(scanner.nextLine());
-    }
-
-    public static List<Point> inputCoordinates(String input) {
         try {
-            input = StringUtils.deleteWhitespace(input);
+            String input = StringUtils.deleteWhitespace(scanner.nextLine());
             checkInputPointsFormat(input);
-            return generatePoints(input);
+            return input;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputCoordinates();
@@ -38,24 +28,5 @@ public class InputView {
         if (!matcher.matches()) {
             throw new IllegalArgumentException(Message.ERROR_INVALID_COORDINATES);
         }
-    }
-
-    private static List<Point> generatePoints(String input) {
-        String[] inputPoints = input.split(POINT_DELIMITER);
-
-        return Arrays.stream(inputPoints)
-                .map(InputView::generatePoint)
-                .collect(Collectors.toList())
-                ;
-    }
-
-    private static Point generatePoint(String inputPoint) {
-        Matcher matcher = PATTERN_POINT.matcher(inputPoint);
-        if (matcher.find()) {
-            int x = Integer.parseInt(matcher.group(1));
-            int y = Integer.parseInt(matcher.group(2));
-            return new Point(x, y);
-        }
-        throw new IllegalArgumentException(Message.ERROR_INVALID_COORDINATES);
     }
 }
