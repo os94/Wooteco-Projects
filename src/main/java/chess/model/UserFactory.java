3165a5ce81fcf7a99c1da506095b911c7e3195b4
os94 <ss1511@naver.com>
2019-06-19 17:28:09 +0900

    [ADD] User, UserFactory 클래스 및 테스트 추가

---
+++ src/main/java/chess/model/UserFactory.java
@@ -0,0 +1,38 @@
+package chess.model;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserFactory {
+    private static List<Chess> defaultChesses = Arrays.asList(
+            Chess.ROOK, Chess.KNIGHT, Chess.BISHOP, Chess.QUEEN,
+            Chess.KING, Chess.BISHOP, Chess.KNIGHT, Chess.ROOK);
+
+    public static User createWhite() {
+        Map<Point, Chess> chesses = new HashMap<>();
+        initializeChesses(chesses, 1);
+        initializePawns(chesses, 2);
+        return new User(chesses);
+    }
+
+    public static User createBlack() {
+        Map<Point, Chess> chesses = new HashMap<>();
+        initializeChesses(chesses, 8);
+        initializePawns(chesses, 7);
+        return new User(chesses);
+    }
+
+    private static void initializePawns(Map<Point, Chess> chesses, int y) {
+        for (int i = 1; i <= 8; i++) {
+            chesses.put(Point.of(i, y), Chess.Pawn);
+        }
+    }
+
+    private static void initializeChesses(Map<Point, Chess> chesses, int y) {
+        for (int i = 1; i <= 8; i++) {
+            chesses.put(Point.of(i, y), defaultChesses.get(i - 1));
+        }
+    }
+}
