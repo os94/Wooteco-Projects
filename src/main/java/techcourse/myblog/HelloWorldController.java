87421e02a69a0a2850cc2090aa53bb5841159ae8
os94 <ss1511@naver.com>
2019-07-09 16:14:04 +0900

    [Fix] Pass ArticleControllerTest, HelloWorldControllerTest

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,29 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+    @ResponseBody
+    @GetMapping("/helloworld")
+    public String helloworld(String blogName) {
+        return blogName;
+    }
 
+    @GetMapping("/")
+    public String helloworld2(String blogName, Model model) {
+        model.addAttribute("blogName", blogName);
+        return "index";
+    }
+
+    @ResponseBody
+    @PostMapping("/helloworld")
+    public String helloworld3(@RequestBody String body) {
+        return body;
+    }
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,16 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String aArticleCreateForm() {
+        return "article-edit";
+    }
 }
