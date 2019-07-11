841ad0b6783363cade6b073a4f529c897e24fc8c
os94 <ss1511@naver.com>
2019-07-11 13:30:22 +0900

    [Fix] create_article Test

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,10 +1,13 @@
 package techcourse.myblog.web;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -34,8 +37,7 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void create_article() throws Exception {
-        //TODO 브라운한테 물어보기
+    void create_article() {
         String title = "목적의식 있는 연습을 통한 효과적인 학습";
         String coverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
         String contents = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 '선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가?'였다.";
@@ -54,17 +56,7 @@ public class ArticleControllerTests {
                     String body = new String(response.getResponseBody());
                     assertThat(body.contains(title)).isTrue();
                     assertThat(body.contains(coverUrl)).isTrue();
-                    assertThat(body.contains(contents)).isTrue();
+                    assertThat(body.contains(StringEscapeUtils.escapeJava(contents))).isTrue();
                 });
-
-    }
-
-    private String decode(int ch) {
-        try {
-            // return URLDecoder.decode(title, "UTF-8");
-            return "\\u" + Integer.toHexString((ch | 0x10000)).substring(1);
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
