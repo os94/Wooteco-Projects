8326074aef3b20c82db8dc35f3aa2729fbdf7c89
os94 <ss1511@naver.com>
2019-07-13 19:27:10 +0900

    [REF] ArticleControllerTests 중복제거

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -7,10 +7,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
@@ -19,63 +18,74 @@ public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static final String URI_ARTICLES = "/articles";
+    private static final String LOCATION = "location";
     private static final int ARTICLE_ID = 1;
 
     @BeforeEach
     void 게시글_작성_확인() {
-        webTestClient.post()
-                .uri("/articles")
-                .body(fromFormData("title", "test-title").with("contents", "test-contents"))
-                .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*articles.*");
+        statusIsFoundAndMatchHeaderValue(
+                HttpMethod.POST,
+                URI_ARTICLES,
+                LOCATION,
+                ".*articles.*"
+        );
     }
 
     @Test
     void index_페이지_조회() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
+        statusIsOk(HttpMethod.GET, "/");
     }
 
     @Test
     void 게시글_작성_페이지_확인() {
-        webTestClient.get().uri("/writing")
-                .exchange()
-                .expectStatus().isOk();
+        statusIsOk(HttpMethod.GET, "/writing");
     }
 
     @Test
     void 게시글_조회() {
-        webTestClient.get()
-                .uri("/articles/" + ARTICLE_ID)
-                .exchange()
-                .expectStatus().isOk();
+        statusIsOk(HttpMethod.GET, URI_ARTICLES + "/" + ARTICLE_ID);
     }
 
     @Test
     void 게시글_수정_페이지_확인() {
-        webTestClient.get()
-                .uri("/articles/" + ARTICLE_ID + "/edit")
-                .exchange()
-                .expectStatus().isOk();
+        statusIsOk(HttpMethod.GET, URI_ARTICLES + "/" + ARTICLE_ID + "/edit");
     }
 
     @Test
     void 게시글_수정_확인() {
-        webTestClient.put()
-                .uri("/articles/" + ARTICLE_ID)
-                .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*articles.*");
+        statusIsFoundAndMatchHeaderValue(
+                HttpMethod.PUT,
+                URI_ARTICLES + "/" + ARTICLE_ID,
+                LOCATION,
+                ".*articles.*"
+        );
     }
 
     @AfterEach
     void 게시글_삭제_확인() {
-        webTestClient.delete()
-                .uri("/articles/" + ARTICLE_ID)
+        statusIsFoundAndMatchHeaderValue(
+                HttpMethod.DELETE,
+                URI_ARTICLES + "/" + ARTICLE_ID,
+                LOCATION,
+                ".*/"
+        );
+    }
+
+    private void statusIsOk(HttpMethod httpMethod, String uri) {
+        webTestClient.method(httpMethod)
+                .uri(uri)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*/");
+                .expectStatus()
+                .isOk();
+    }
+
+    private void statusIsFoundAndMatchHeaderValue(HttpMethod httpMethod, String uri, String name, String pattern) {
+        webTestClient.method(httpMethod)
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isFound()
+                .expectHeader().valueMatches(name, pattern);
     }
 }
