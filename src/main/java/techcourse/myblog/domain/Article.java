8c9effe02afd169a90ecdd8d654e7bb6ac330cdd
os94 <ss1511@naver.com>
2019-07-16 15:09:50 +0900

    Refactor : Article class의 entity 속성 추가

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,7 +1,16 @@
 package techcourse.myblog.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Article {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String title;
     private String coverUrl;
     private String contents;
@@ -12,11 +21,11 @@ public class Article {
         this.contents = contents;
     }
 
-    public boolean matchId(int id) {
+    public boolean matchId(long id) {
         return this.id == id;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -32,7 +41,7 @@ public class Article {
         return contents;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
