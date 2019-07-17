b3dc950d603f7e67e824d796b0380c3b6e8307ca
os94 <ss1511@naver.com>
2019-07-17 18:22:51 +0900

    Add : 회원가입 기능 구현

--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -1,13 +1,34 @@
 package techcourse.myblog.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import techcourse.myblog.domain.UserRepository;
+import techcourse.myblog.domain.UserRequestDto;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/signup")
     public String index() {
         return "signup";
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public String createUser(@Valid UserRequestDto userRequestDto) {
+        userRepository.save(userRequestDto.toEntity());
+        return "redirect:/login";
     }
 }
--- src/test/java/techcourse/myblog/web/UserControllerTests.java
+++ src/test/java/techcourse/myblog/web/UserControllerTests.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
@@ -17,9 +20,25 @@ public class UserControllerTests {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static int USER_ID;
+
     @Test
     void 회원가입_페이지_이동_확인() {
         statusIsOk(HttpMethod.GET, "/signup");
+    }
+
+    @Test
+    void 회원가입_확인() {
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("name", "sean")
+                        .with("email", "sean@gmail.com")
+                        .with("password", "Woowahan123!"))
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().valueMatches(HttpHeaders.LOCATION, ".*login.*");
     }
 
     private void statusIsOk(HttpMethod httpMethod, String uri) {
