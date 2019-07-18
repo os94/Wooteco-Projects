b912a9516e7ae370c5eba6575f81951c29a41ba3
os94 <ss1511@naver.com>
2019-07-18 14:52:06 +0900

    Add : UserControllerTests 회원가입 검증 테스트 추가

--- src/test/java/techcourse/myblog/web/UserControllerTests.java
+++ src/test/java/techcourse/myblog/web/UserControllerTests.java
@@ -12,15 +12,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static techcourse.myblog.domain.UserRequestDto.*;
+
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTests {
-
+    
     @Autowired
     private WebTestClient webTestClient;
-
-    private static int USER_ID;
 
     @Test
     void 회원가입_페이지_이동_확인() {
@@ -41,11 +42,88 @@ public class UserControllerTests {
                 .expectHeader().valueMatches(HttpHeaders.LOCATION, ".*login.*");
     }
 
+    @Test
+    void name이_2자_미만인_경우() {
+        checkValidationWith(NAME_LENGTH_ERROR, "s", "sean@gmail.com", "Woowahan123!");
+    }
+
+    @Test
+    void name이_10자_초과인_경우() {
+        checkValidationWith(NAME_LENGTH_ERROR, "qwerasdfzxc", "sean@gmail.com", "Woowahan123!");
+    }
+
+    @Test
+    void name에_숫자가_포함된_경우() {
+        checkValidationWith(NAME_FORMAT_ERROR, "sean123", "sean@gmail.com", "Woowahan123!");
+    }
+
+    @Test
+    void name에_특수문자가_포함된_경우() {
+        checkValidationWith(NAME_FORMAT_ERROR, "sean!", "sean@gmail.com", "Woowahan123!");
+    }
+    
+    
+    @Test
+    void name에_공백이_입력된_경우() {
+        checkValidationWith(NAME_BLANK_ERROR, " ", "sean@gmail.com", "Woowahan123!");
+    }
+
+    @Test
+    void email_양식이_잘못된_경우() {
+        checkValidationWith(EMAIL_FORMAT_ERROR, "sean!", "sean_gmail.com", "Woowahan123!");
+    }
+
+    @Test
+    void email이_공백인_경우() {
+        checkValidationWith(EMAIL_BLANK_ERROR, "sean!", "", "Woowahan123!");
+    }
+
+    @Test
+    void password에_소문자가_포함되지_않은_경우() {
+        checkValidationWith(PASSWORD_FORMAT_ERROR, "sean!", "sean@gmail.com", "WOOWAHAN123!");
+    }
+
+    @Test
+    void password에_대문자가_포함되지_않은_경우() {
+        checkValidationWith(PASSWORD_FORMAT_ERROR, "sean!", "sean@gmail.com", "woowahan123!");
+    }
+
+    @Test
+    void password에_숫자가_포함되지_않은_경우() {
+        checkValidationWith(PASSWORD_FORMAT_ERROR, "sean!", "sean@gmail.com", "Woowahan!");
+    }
+
+    @Test
+    void password에_특수문자가_포함되지_않은_경우() {
+        checkValidationWith(PASSWORD_FORMAT_ERROR, "sean!", "sean@gmail.com", "Woowahan123");
+    }
+
+    @Test
+    void password에_공백이_입력된_경우() {
+        checkValidationWith(PASSWORD_BLANK_ERROR, "sean!", "sean@gmail.com", " ");
+    }
+
     private void statusIsOk(HttpMethod httpMethod, String uri) {
         webTestClient.method(httpMethod)
                 .uri(uri)
                 .exchange()
                 .expectStatus()
                 .isOk();
+    }
+
+    private void checkValidationWith(String errorMessage, String name, String email, String password) {
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("name", name)
+                        .with("email", email)
+                        .with("password", password))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().consumeWith(response -> {
+            String body = new String(response.getResponseBody());
+            assertThat(body.contains(errorMessage)).isTrue();
+        });
     }
 }
