341395fd7e612e9bf19700fafbb7eaa70018a863
os94 <ss1511@naver.com>
2019-07-19 11:44:54 +0900

    Add : Logger 실습

--- src/test/java/techcourse/myblog/web/MainControllerTests.java
+++ src/test/java/techcourse/myblog/web/MainControllerTests.java
@@ -2,6 +2,8 @@ package techcourse.myblog.web;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,8 +18,16 @@ public class MainControllerTests {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static final Logger log = LoggerFactory.getLogger(MainControllerTests.class);
+    
     @Test
     void index_페이지_조회() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
         statusIsOk(HttpMethod.GET, "/");
     }
 
