5b96206be7e0d6745a35735bac6419d504a88a28
os94 <ss1511@naver.com>
2019-07-30 15:55:14 +0900

    Refactor : CommentController의 RequestMapping 중복제거

--- src/main/java/techcourse/myblog/web/controller/CommentController.java
+++ src/main/java/techcourse/myblog/web/controller/CommentController.java
@@ -2,10 +2,7 @@ package techcourse.myblog.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
@@ -18,6 +15,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 
 @Controller
+@RequestMapping("/articles/{articleId}/comments")
 public class CommentController {
     private final CommentService commentService;
     private final ArticleService articleService;
@@ -28,20 +26,20 @@ public class CommentController {
         this.articleService = articleService;
     }
 
-    @PostMapping("/comments")
+    @PostMapping("")
     public RedirectView createComment(CommentRequestDto commentRequestDto, HttpSession httpSession) {
         Comment comment = convert(commentRequestDto, httpSession);
         commentService.save(comment);
         return new RedirectView("/articles/" + commentRequestDto.getArticleId());
     }
 
-    @PutMapping("/articles/{articleId}/comments/{commentId}")
+    @PutMapping("/{commentId}")
     public RedirectView updateComment(@PathVariable long articleId, @PathVariable long commentId, String contents) {
         commentService.update(commentId, contents);
         return new RedirectView("/articles/" + articleId);
     }
 
-    @DeleteMapping("/comments/{commentId}")
+    @DeleteMapping("/{commentId}")
     public RedirectView deleteComment(@PathVariable long commentId, long articleId) {
         commentService.deleteById(commentId);
         return new RedirectView("/articles/" + articleId);
