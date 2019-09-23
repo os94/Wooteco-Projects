1a325a667a49bdbd54e551e67df31b265d714597
os94 <ss1511@naver.com>
2019-09-23 16:07:29 +0900

    refactor: HashMap -> EnumMap in HttpMethod Enum

--- src/main/java/http/common/HttpMethod.java
+++ src/main/java/http/common/HttpMethod.java
@@ -1,20 +1,20 @@
 package http.common;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 public enum HttpMethod {
     GET, POST, PUT, DELETE;
 
-    private static final Map<String, HttpMethod> httpMethods = new HashMap<>();
+    private static final Map<HttpMethod, String> httpMethods = new EnumMap<>(HttpMethod.class);
 
     static {
         for (HttpMethod httpMethod : HttpMethod.values()) {
-            httpMethods.put(httpMethod.name(), httpMethod);
+            httpMethods.put(httpMethod, httpMethod.name());
         }
     }
 
     public static boolean matches(String method) {
-        return httpMethods.containsKey(method);
+        return httpMethods.containsValue(method);
     }
 }
