9da39898f586b0a40864611b1edd539ab51e44a8
os94 <ss1511@naver.com>
2019-10-01 13:19:34 +0900

    test: Reflection을 이용한 @Test 애노테이션 메소드 실행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,11 +2,25 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> methods = Arrays.asList(clazz.getDeclaredMethods());
+        methods.stream()
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
