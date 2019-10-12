9b865de474991d1659b18cea5425656b7a49f534
os94 <ss1511@naver.com>
2019-10-12 19:25:28 +0900

    refactor: UserApiController, JsonUtils 리팩토링

--- nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
@@ -4,17 +4,34 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.exception.ObjectMapperException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class JsonUtils {
-    public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
+
+    private static ObjectMapper objectMapper;
+
+    public static <T> T toObject(String json, Class<T> clazz) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper = new ObjectMapper();
             objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
                     .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
                     .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
                     .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
             return objectMapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new ObjectMapperException(e);
+        }
+    }
+
+    public static <T> T toObject(HttpServletRequest request, Class<T> clazz) {
+        try {
+            objectMapper = new ObjectMapper();
+            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
+                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+            return objectMapper.readValue(request.getInputStream(), clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
--- slipp/src/main/java/slipp/controller/UserApiController.java
+++ slipp/src/main/java/slipp/controller/UserApiController.java
@@ -5,26 +5,20 @@ import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import nextstep.web.annotation.ResponseBody;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import slipp.domain.User;
 import slipp.dto.UserUpdatedDto;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 @Controller
 public class UserApiController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserApiController.class);
-
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
     public @ResponseBody
     User create(HttpServletRequest request, HttpServletResponse response) {
-        User user = parseBody(request, User.class);
+        User user = JsonUtils.toObject(request, User.class);
         DataBase.addUser(user);
 
         response.setStatus(HttpServletResponse.SC_CREATED);
@@ -37,31 +31,18 @@ public class UserApiController {
     User find(HttpServletRequest request, HttpServletResponse response) {
         String userId = request.getParameter("userId");
         User user = DataBase.findUserById(userId);
-
-        response.setStatus(HttpServletResponse.SC_OK);
         return user;
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
     public @ResponseBody
     User update(HttpServletRequest request, HttpServletResponse response) {
-        UserUpdatedDto userUpdatedDto = parseBody(request, UserUpdatedDto.class);
+        UserUpdatedDto userUpdatedDto = JsonUtils.toObject(request, UserUpdatedDto.class);
         String userId = request.getParameter("userId");
         User user = DataBase.findUserById(userId);
         user.update(new User(userId, userUpdatedDto.getPassword(), userUpdatedDto.getName(), userUpdatedDto.getEmail()));
 
-        response.setStatus(HttpServletResponse.SC_OK);
         response.addHeader("Location", "/api/users?userId=" + user.getUserId());
         return user;
-    }
-
-    // TODO: 2019-10-12 Refactoring
-    private <T> T parseBody(HttpServletRequest request, Class<T> clazz) {
-        try {
-            return JsonUtils.toObject(request.getReader().lines().collect(Collectors.joining()), clazz);
-        } catch (IOException e) {
-            logger.error("Error occurred during parsing json body", e);
-            throw new RuntimeException(e);
-        }
     }
 }
