e1e93738763c9d26f00d730a88d8060a07dd2850
os94 <ss1511@naver.com>
2019-10-22 14:48:44 +0900

    refactor: ConnectionManager에서 db설정 분리

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,22 +3,33 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
 
     public static DataSource getDataSource() {
+        Properties properties = readProperties();
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(properties.getProperty("jdbc.driverClass"));
+        ds.setUrl(properties.getProperty("jdbc.url"));
+        ds.setUsername(properties.getProperty("jdbc.username"));
+        ds.setPassword(properties.getProperty("jdbc.password"));
         return ds;
+    }
+
+    private static Properties readProperties() {
+        Properties properties = new Properties();
+        String resource = "src/main/resources/db.properties";
+        try {
+            properties.load(new FileReader(resource));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 
     public static Connection getConnection() {
