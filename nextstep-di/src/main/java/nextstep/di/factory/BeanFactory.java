b40c18f590d65258a47e1488d34accdc89f511ed
os94 <ss1511@naver.com>
2019-11-06 16:46:51 +0900

    feat: Bean 초기화 기능 추가
    Bean 스캔&등록 과정을 추가

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -3,7 +3,12 @@ package nextstep.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +29,38 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstanticateBeans.forEach(this::jaegui);
+    }
 
+    public void jaegui(Class<?> clazz) {
+        if (beans.containsKey(clazz)) {
+            return;
+        }
+
+        if (clazz.isInterface()) {
+            clazz = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+            try {
+                Object o = clazz.getDeclaredConstructor().newInstance();
+                beans.put(clazz, o);
+                return;
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        if (injectedConstructor.getParameterTypes().length == 0) {
+            beans.put(clazz, BeanUtils.instantiateClass(injectedConstructor));
+            return;
+        } else {
+            List<Object> params = new ArrayList<>();
+            for (Class<?> parameterType : injectedConstructor.getParameterTypes()) {
+                jaegui(parameterType);
+                Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterType, preInstanticateBeans);
+                params.add(beans.get(concreteClass));
+            }
+            Object o = BeanUtils.instantiateClass(injectedConstructor, params.toArray());
+            beans.put(clazz, o);
+        }
     }
 }
